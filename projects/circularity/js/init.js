dc9494dac6b137b5d62ccc3053003040f36ab49e
KaelKite <kitekael@gmail.com>
2021-12-16 00:34:19 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -48,20 +48,16 @@ var init = function (window) {
         function update() {
             // TODO 4 : Update the circle's position //
            
-            physikz.updatePosition(circles[0])
-            physikz.updatePosition(circles[1])
-            physikz.updatePosition(circles[2])
-            physikz.updatePosition(circles[3])
-            physikz.updatePosition(circles[4])
+           
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-            game.checkCirclePosition(circles[0])
-            game.checkCirclePosition(circles[1])
-            game.checkCirclePosition(circles[2])
-            game.checkCirclePosition(circles[3])
-            game.checkCirclePosition(circles[4])
+           
 
             // TODO 9 : Iterate over the array
-           
+           for (var eachCircle = 0; eachCircle < circles.length; eachCircle++){
+               var eachValue = circles[eachCircle];
+               physikz.updatePosition(eachValue);
+               game.checkCirclePosition(eachValue)
+           }
             
         }
     
