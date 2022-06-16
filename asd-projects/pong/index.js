fe3937aa00ba3a1ddec8d978689234e8af019d91
KaelKite <kitekael@gmail.com>
2022-06-16 21:22:10 +0000

    saving all

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -15,10 +15,48 @@ function runProgram(){
     UP: 38,
     DOWN: 40,
   }
-  var WD = {
+  var WS = {
     W: 87,
-    D: 68,
+    S: 83,
   }
+  //Keydown/Keyup function
+  function handleKeyDown(event) {
+    var keycode = event.which;
+    console.log(keycode);
+     if (event.which === KEY.UP){
+      speedY = -5;
+    }
+    else if (event.which === KEY.DOWN){
+      speedY = 5;
+    }
+  }
+  function handleKeyUp (event){
+     if (event.which === KEY.UP){
+      speedY = 0;
+    }
+    else if (event.which === KEY.DOWN){
+      speedY = 0;
+    }
+  }
+  function handleKeyDown2(event) {
+    var keycode = event.which;
+    console.log(keycode);
+     if (event.which === WS.W){
+      speedY = -5;
+    }
+    else if (event.which === WS.S){
+      speedY = 5;
+    }
+  }
+  function handleKeyUp2(event){
+    if (event.which === WS.W){
+     speedY = 0;
+   }
+   else if (event.which === WS.S){
+     speedY = 0;
+   }
+ }
+ 
   // Game Item Objects
   function GameItem(elementId) {
     var gameItem = {};
@@ -33,12 +71,15 @@ function runProgram(){
     }
 
     var obj = GameItem("#ball")
-
+    var obj1 = GameItem("#player1")
+    var obj2 = GameItem("#player2")
     
   // one-time setup
-  let interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
-
+  let interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)                           
+  $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keyup', handleKeyUp);
+  $(document).on('keydown', handleKeyDown2);
+  $(document).on('keyup', handleKeyUp2);
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -48,7 +89,10 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
+    repositionGameItem();
+    redrawGameItem();    
+    repositionGameItem2(); 
+    redrawGameItem2();    
 
   }
   
@@ -62,7 +106,22 @@ function runProgram(){
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
-
+  function repositionGameItem(){
+    positionX += speedX;
+    positionY += speedY;
+   }
+   function repositionGameItem2(){
+    playerX += playerSpeedX;
+    playerY += playerSpeedY;
+   }
+   function redrawGameItem(){
+    $("#player1").css("left", positionX);
+    $("#player1").css("top", positionY);
+   }
+   function redrawGameItem2(){
+    $("#player2").css("left", playerX);
+    $("#player2").css("top", playerY);
+   }
   
   function endGame() {
     // stop the interval timer
